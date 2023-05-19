f06a9ed5bd56ea592959b20c9fff9a704ad79946
Malik Omar <91993656+abdimalik2010@users.noreply.github.com>
2023-05-20 00:42:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     storage_account_name = "hasstorage123"
     container_name       = "tfterraform"
     key                  = "terraform.tfstate"
-    access_key           = "RPX9N5yeWDYeEZBqfC7W4zQMXrhO1c9p4Owf6/0dlxQw+n52E4PvnhgHUUSvY8JSKLOBaJTBWGC++AStbNSNTw=="
+    
   }
   required_providers {
     azurerm = {
