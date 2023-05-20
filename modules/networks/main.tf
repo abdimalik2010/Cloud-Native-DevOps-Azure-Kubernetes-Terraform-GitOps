220699dd352fc8e65d01f9c507f5a32bd6998fc1
Malik Omar <91993656+abdimalik2010@users.noreply.github.com>
2023-05-21 00:06:02 +0200

    Update main.tf

--- modules/networks/main.tf
+++ modules/networks/main.tf
@@ -9,12 +9,12 @@ resource "azurerm_subnet" "primary_subnet" {
   name                 = "primary_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.k8.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "secondary_subnet" {
   name                 = "secondary_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.k8.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
