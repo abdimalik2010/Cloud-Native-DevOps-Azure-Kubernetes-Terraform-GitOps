f86ced5695ae102d08024d360a1657688a57c1d9
Malik Omar <91993656+abdimalik2010@users.noreply.github.com>
2023-05-26 20:07:18 +0200

    Update main.tf

--- modules/kubernetes_cluster/main.tf
+++ modules/kubernetes_cluster/main.tf
@@ -19,10 +19,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size    = "Standard_D2_v2"
   }
 
-
  service_principal {
-   client_id = var.ARM_CLIENT_ID
-   client_secret = var.ARM_CLIENT_SECRET
- }
-
+    client_id     = var.ARM_CLIENT_ID
+    client_secret = var.ARM_CLIENT_SECRET
+  }
 }
