055c6f65567c2e189aab821005446b8ed16584ea
Malik Omar <91993656+abdimalik2010@users.noreply.github.com>
2023-05-27 02:21:23 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "ARM_CLIENT_ID" {
-  type = string
-  sensitive = true
-}
+#variable "ARM_CLIENT_ID" {
+ # type = string
+  #sensitive = true
+#}
 variable "ARM_CLIENT_SECRET" {
   type = string
   sensitive = true
